894d12a57b444548624040b89ba86494c9368a3a
tom-daly <tom.m.daly@gmail.com>
2017-08-04 17:45:09 -0400

    adding spsave-demo-1, includes gulp-watch for new files and gulp-sass for sass compilation

---
+++ spsave-demo-1/coreOptions.js
@@ -0,0 +1,9 @@
+var filesMetaData = require('./filesMetaData.js');
+
+module.exports = {
+    siteUrl: "",
+    notification: true,
+    checkin: true,
+    checkinType: 1, //0 minor, 1 major, 2 overwrite
+    filesMetaData: filesMetaData 
+}
