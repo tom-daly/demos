2d62664e5fd73a44830a4d135e82a3ce5ef42183
Thomas Daly <tom.m.daly@gmail.com>
2018-03-21 20:27:33 -0400

    update spfx react demo

--- o365-bootcamp/spfx-office-ui-demo/src/webparts/helloWorld/components/HelloWorld.tsx
+++ o365-bootcamp/spfx-office-ui-demo/src/webparts/helloWorld/components/HelloWorld.tsx
@@ -1,24 +1,71 @@
-import * as React from 'react';
-import styles from './HelloWorld.module.scss';
-import { IHelloWorldProps } from './IHelloWorldProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./HelloWorld.module.scss";
+import { IHelloWorldProps } from "./IHelloWorldProps";
+import { escape } from "@microsoft/sp-lodash-subset";
+import { DefaultButton } from "office-ui-fabric-react/lib/Button";
+import { Dropdown, IDropdownProps } from "office-ui-fabric-react/lib/Dropdown";
+import { TextField } from "office-ui-fabric-react/lib/TextField";
+import { Icon } from "office-ui-fabric-react/lib/Icon";
+import { Checkbox } from "office-ui-fabric-react/lib/Checkbox";
+import { initializeIcons } from "@uifabric/icons";
+import {
+  DatePicker,
+  DayOfWeek,
+  IDatePickerStrings
+} from "office-ui-fabric-react/lib/DatePicker";
+
+initializeIcons();
 
 export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
   public render(): React.ReactElement<IHelloWorldProps> {
     return (
       <div className={styles.helloWorld}>
-        <div className={styles.container}>
-          <div className={`ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`}>
-            <div className="ms-Grid-col ms-lg10 ms-xl8 ms-xlPush2 ms-lgPush1">
-              <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
-              <p className="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
-              <p className="ms-font-l ms-fontColor-white">{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={styles.button}>
-                <span className={styles.label}>Learn more</span>
-              </a>
-            </div>
-          </div>
+        <div className="ms-bgColor-themeDarker">
+          <h1 className="ms-font-xxl ms-fontColor-white" style={{ padding: 10 }}>Submit Time</h1>
         </div>
+
+        <div className={styles.row}>
+          <Dropdown
+            placeHolder="Choose a project..."
+            options={[
+              { key: "Project A", text: "Project A" },
+              { key: "Project B", text: "Project B" },
+              { key: "Project C", text: "Project C" }
+            ]}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <DatePicker label="Date" placeholder="Select a date..." />
+        </div>
+
+        <div className={styles.row}>
+          <TextField label="Hours" />
+        </div>
+
+        <div className={styles.row}>
+          <TextField label="Minutes" />
+        </div>
+
+        <div className={styles.row}>
+          <TextField label="Notes" multiline rows={4} />
+        </div>
+
+        <div className={styles.row}>
+          <Checkbox label="Billable" checked />
+        </div>
+
+        <DefaultButton
+          text="Submit"
+          primary
+          iconProps={{ iconName: "Accept" }}
+          style={{ marginRight: 10 }}
+        />
+        <DefaultButton
+          text="Cancel"
+          primary
+          iconProps={{ iconName: "Cancel" }}
+        />
       </div>
     );
   }
