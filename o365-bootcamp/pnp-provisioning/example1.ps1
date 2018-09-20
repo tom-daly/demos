4b6a612b707623e9b38f5a4856169a525adffd66
Thomas Daly <tom.m.daly@gmail.com>
2018-09-19 23:47:43 -0400

    updates on provisiong

--- o365-bootcamp/pnp-provisioning/example1.ps1
+++ o365-bootcamp/pnp-provisioning/example1.ps1
@@ -1,26 +1,20 @@
-$configFile = "example1.json";
+## URL MANAGER
+## https://github.com/SharePoint/PnP-PowerShell/wiki/How-to-use-the-Windows-Credential-Manager-to-ease-authentication-with-PnP-PowerShell
 
-if((Test-Path $configFile) -eq $false) {
-    $siteUrl = Read-Host -Prompt "Enter the site url"
-    $username = Read-Host -Prompt "Enter the username"
-    $securePassword = Read-Host -Prompt "Enter your tenant password" -AsSecureString | ConvertFrom-SecureString
-    @{username=$username;securePassword=$securePassword;siteUrl=$siteUrl} | ConvertTo-Json | Out-File $configFile
-}
-
-$configObject = Get-Content $configFile | ConvertFrom-Json
-$password = $configObject.securePassword | ConvertTo-SecureString
-$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $configObject.username, $password
-Connect-PnPOnline -url $configObject.siteUrl -Credentials $credentials
+## SETUP / TEAR DOWN
+# Connect-SPOService https://bandrdev-admin.sharepoint.com
+# Remove-SPOSite https://bandrdev.sharepoint.com/sites/PnPTargetA
 
 ## Example 1
 ## Generate the template of 
 ## https://bandrdev.sharepoint.com/sites/PnPTemplateA
+Connect-PnPOnline -url "https://bandrdev.sharepoint.com/sites/PnPTemplateA"
 Get-PnPProvisioningTemplate -Out .\example1.xml
 
 ## Connect to the Target Site
-## https://bandrdev.sharepoint.com/sites/PnPTargetB
-# Remove-SPOSite https://bandrdev.sharepoint.com/sites/PnPTargetA
-Connect-PnPOnline -url "https://bandrdev.sharepoint.com/sites/PnPTargetA" -Credentials $credentials
+## https://bandrdev.sharepoint.com/sites/PnPTargetA
+
+Connect-PnPOnline -url "https://bandrdev.sharepoint.com/sites/PnPTargetA"
 
 ## Apply template to the new site
 Apply-PnPProvisioningTemplate .\example1.xml
